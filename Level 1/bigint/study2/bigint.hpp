e1091591a6e44bc5dea52c6c1373f844dca1bda4
Muhammad Bani Yamin <mbani-ya@u82z01s01.42kl.edu.my>
2025-11-27 19:14:53 +0800

    weird bug

---
+++ Level 1/bigint/study2/bigint.hpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bigint.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbani-ya <mbani-ya@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/27 10:41:57 by mbani-ya          #+#    #+#             */
+/*   Updated: 2025/11/27 18:36:47 by mbani-ya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BIGINT_HPP
+# define BIGINT_HPP
+
+#include <string>
+#include <vector>
+
+class bigint {
+private:
+	std::vector<int> digits;
+	void	trim();
+	int		to_int() const;
+public:
+	bigint();
+	bigint(unsigned long long n);
+	bigint(const std::string& s);
+	bigint(const bigint& other);
+	bigint&	operator=(const bigint& other);
+	~bigint();
+	//addition,substraction
+	bigint	operator+(const bigint& other) const;
+	bigint	operator+=(const bigint& other);
+	//digitshift
+	bigint	operator<<(const bigint& other) const;
+	bigint	operator<<(const int shift) const;
+	bigint&	operator<<=(const bigint& other);
+	bigint&	operator<<=(const int shift);
+	bigint	operator>>(const bigint& other) const;
+	bigint	operator>>(const int shift) const;
+	bigint&	operator>>=(const bigint& other);
+	bigint&	operator>>=(const int shift);
+	//increment, decrement
+	bigint&	operator++();
+	bigint	operator++(int);
+	friend	std::ostream& operator<<(std::ostream& os,const bigint& other);
+	//comparison
+	bool	operator<(const bigint& other) const;
+	bool	operator>(const bigint& other) const;
+	bool	operator<=(const bigint& other) const;
+	bool	operator>=(const bigint& other) const;
+	bool	operator==(const bigint& other) const;
+	bool	operator!=(const bigint& other) const;
+};
+
+
+#endif
